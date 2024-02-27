4c6591877a70ee2f29447fa591cac86d9ab97fcb
Joselyn Obando <obandofernandez1@gmail.com>
2024-02-27 16:30:46 +0100

    Agregando routes Products

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('products.index');
+});
+
+Route::group(['prefix' => 'products'], function () {
+    Route::get('/', [ProductsController::class, 'index'])->name('products.index');
+    Route::get('/create', [ProductsController::class, 'create'])->name('products.create');//->middleware(['auth', 'admin']);
+    Route::post('/', [ProductsController::class, 'store'])->name('products.store');//->middleware(['auth','admin']);
+    Route::get('/{product}', [ProductsController::class, 'show'])->name('products.show');
+    Route::get('/{product}/edit', [ProductsController::class, 'edit'])->name('products.edit');//->middleware(['auth','admin']);
+    Route::put('/{product}', [ProductsController::class, 'update'])->name('products.update');//->middleware(['auth','admin']);
+    Route::delete('/{product}', [ProductsController::class, 'destroy'])->name('products.destroy');//->middleware(['auth','admin']);
+    Route::get('/{product}/edit-image', [ProductsController::class, 'editImage'])->name('products.editImage');//->middleware(['auth','admin']);
+    Route::patch('/{product}/edit-image', [ProductsController::class, 'updateImage'])->name('products.updateImage');//->middleware(['auth','admin']);
 });
 
 Route::group(['prefix' => 'categories'], function () {

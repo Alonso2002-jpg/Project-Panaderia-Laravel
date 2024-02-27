75eb23b74f9e3de32da59a6ab2266d2d71bc417d
MiguelZanottto <migzanotto18@hotmail.com>
2024-02-27 20:08:33 +0100

    Creado modelo, migracion y seeder de Client

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CategoryTableSeeder::class);
         $this->call(ProductTableSeeder::class);
         $this->call(ProviderTableSeeder::class);
+        $this->call(ClientTableSeeder::class);
     }
 }

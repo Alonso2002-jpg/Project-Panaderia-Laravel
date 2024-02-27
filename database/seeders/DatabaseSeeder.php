3cab9e49b44fe57e58827bfc15b33e8658274c84
MiguelZanottto <migzanotto18@hotmail.com>
2024-02-27 22:54:37 +0100

    Creamos los seeders de OrderLine

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ProductTableSeeder::class);
         $this->call(AddressTableSeeder::class);
         $this->call(ClientTableSeeder::class);
+        $this->call(OrderLineTableSeeder::class);
     }
 }

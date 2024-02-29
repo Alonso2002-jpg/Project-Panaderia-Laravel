cf44d1f96e7e8b4de17b9f83dd4460b89ec12424
kevin <kevinber783@gmail.com>
2024-02-29 21:57:54 +0100

    se modifica las migraciones del staff

--- database/migrations/2024_02_27_164022_create_staff_table.php
+++ database/migrations/2024_02_27_164022_create_staff_table.php
@@ -1,22 +1,23 @@
 <?php
 
+use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class {
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('staff', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid');
+            $table->uuid('id')->primary();
             $table->string('name');
-            $table->string('dni');
+            $table->string('dni')->unique();
+            $table->string('email')->unique();
             $table->string('lastname');
             $table->date('startDate');
-            $table->date('endDate');
-            $table->date('updateDate');
-            $table->date('creationDate');
+            $table->date('endDate')->nullable();
             $table->string('image');
+            $table->enum('role', ['baker', 'cashier', 'manager', 'steward', 'cleaner', 'pastry_chef', 'inventory_manager'])->nullable()->default('baker');
+            $table->boolean('isDelete')->default(false);
             $table->timestamps();
         });
     }
@@ -24,5 +25,6 @@ return new class {
     public function down(): void
     {
         Schema::dropIfExists('staff');
+
     }
 };

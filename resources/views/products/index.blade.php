27a82d084b38ed623b92a057c11e7d6661469456
Laura Garrido Arredondo <anoobmedic@gmail.com>
2024-03-02 20:21:20 +0100

    Adding shows in products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -31,7 +31,7 @@
                             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                                 <div class="text-center">
                                     <!-- Enlace al detalle del producto -->
-                                    <a class="btn btn-outline-dark mt-auto" href="#product_{{ $product->id }}">See Details</a>
+                                    <a class="btn btn-outline-dark mt-auto" href="{{ route('products.show', $product->id) }}">See Details</a>
                                 </div>
                             </div>
                         </div>
@@ -42,30 +42,4 @@
             </div>
         </div>
     </section>
-
-    <!-- Detalle de los productos -->
-    @foreach($products as $product)
-        <section id="product_{{ $product->id }}" class="py-5">
-            <div class="container px-4 px-lg-5 my-5">
-                <div class="row gx-4 gx-lg-5 align-items-center">
-                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" /></div>
-                    <div class="col-md-6">
-                        <h1 class="display-5 fw-bolder">{{ $product->name }}</h1>
-                        <div class="fs-5 mb-5">
-                            <span>${{ $product->price }}</span>
-                        </div>
-                        <p class="lead">{{ $product->description }}</p>
-                        <div class="d-flex">
-                            <input class="form-control text-center me-3" id="inputQuantity" type="num" value="0" style="max-width: 3rem" />
-                            <button class="btn btn-outline-dark flex-shrink-0" type="button">
-                                <i class="bi-cart-fill me-1"></i>
-                                Add to cart
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-    @endforeach
 @endsection

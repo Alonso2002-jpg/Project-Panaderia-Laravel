acd28d57de0fd50b29ed7083abb865749b557c19
kevin <kevinber783@gmail.com>
2024-03-06 21:28:48 +0100

    test personal

--- tests/Feature/staffControllerTest.php
+++ tests/Feature/staffControllerTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
 class staffControllerTest extends TestCase
 {
     use RefreshDatabase;
@@ -13,14 +17,53 @@ class staffControllerTest extends TestCase
         $this->artisan('db:seed');
     }
 
-    public function test_index_with_admin()
+    public function test_create_view_user()
     {
-        $user = User::factory()->create(['role' => 'admin']);
-        $response = $this->actingAs($user)->get('/staff');
-        $response->assertViewIs('staff.index');
+        $user = User::factory()->create(['role' => 'user']);
+        $response = $this->actingAs($user)->get('/staff/create');
+        $response->assertRedirectToRoute('home');
+        $response->assertStatus(302);
+    }
+
+    public function test_create_view_guest()
+    {
+        $response = $this->get('/staff/create');
+        $response->assertRedirectToRoute('login');
+        $response->assertStatus(302);
+    }
+
+    public function test_create_view_admin()
+    {
+        $adminUser = User::factory()->create(['role' => 'admin']);
+        $response = $this->actingAs($adminUser)->get('/staff/create');
+        $response->assertViewIs('staff.create');
         $response->assertStatus(200);
     }
 
+    public function test_index()
+    {
+        $response = $this->get('/staff');
+        $response->assertViewIs('staff.index');
+        $response->assertViewHas('staff');
+        $response->assertStatus(200);
+    }
+
+    public function test_show_view()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/staff/1');
+        $response->assertViewIs('staff.show');
+        $response->assertViewHas('staff');
+        $response->assertStatus(200);
+    }
+
+    public function test_update_view_user()
+    {
+        $user = User::factory()->create(['role' => 'user']);
+        $response = $this->actingAs($user)->get('/staff/1/edit');
+        $response->assertRedirectToRoute('home');
+        $response->assertStatus(302);
+    }
 
 }
 
